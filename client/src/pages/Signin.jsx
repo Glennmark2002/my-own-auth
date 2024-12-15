6dcd5bea075dd1ad708047cba724272a64bdc988
Glennmark2002 <Glennmark.culibra@gmail.com>
2024-12-15 18:14:03 +0800

    add redux

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice.js";
+import { useDispatch, useSelector } from "react-redux";
+
 
 function Signin() {
 
   const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);  
+
   const navigate = useNavigate();
+  const dispatch = useDispatch();   
   
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id] : e.target.value });   
-    console.log(formData);  
+    setFormData({ ...formData, [e.target.id] : e.target.value });     
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault(); 
 
     try {
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method : 'POST', 
         headers : { 'Content-Type' : 'application/json'},
@@ -23,12 +29,16 @@ function Signin() {
       
       const data = await res.json(); 
       console.log(data) 
-
-      if(data.success === false) return
+      
+      if(data.success === false) {
+        dispatch(signInFailure(data))
+        return
+      }
+      dispatch(signInSuccess(data)); 
       navigate('/');
-
+      
     } catch (error) {
-      console.log(error);  
+      dispatch(signInFailure(error));  
     }
   }
 
